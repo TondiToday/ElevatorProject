e25faa9e202f415c807ee1f356f5899f2f66adeb
Stirk91 <linden.the.lionheart@gmail.com>
2018-03-19 05:15:23 -0500

    Add project files.

---
+++ ElevatorOperations.cpp
@@ -0,0 +1,208 @@
+#include "ElevatorOperations.h"
+#include "Elevator.h"
+#include "Utilities.h"
+#include "User.h"
+
+// need to pass in floor request, if the elevator is at that floor and was the elevator dispatched to that floor
+// not all elevators stops are due to a floor request 
+
+
+void elevator_op(vector<Elevator*> &v, const int& number_of_floors, int& total_users, int& current_time, vector<int>& waiting_times)
+{
+	vector<Elevator*>::iterator el;
+
+	for (el = v.begin(); el != v.end(); ++el)
+	{
+		if (DEBUG == YES)
+		{
+			cout << "\t\t Elevator " << (*el)->get_name() << endl;
+		};
+
+		if ((*el)->get_level() == 0) {
+			(*el)->set_direction(true);
+		}
+		else if ((*el)->get_level() == number_of_floors) {
+			(*el)->set_direction(false);
+		}
+
+
+
+// USER BOARDING LOGIC //
+
+		// if the elevator is stationary and has no floor requests
+		if ((*el)->isBoarding() == true)
+		{
+			if (DEBUG == YES)
+			{
+				cout << endl;
+				cout << "***ELEVATOR Finished Boarding***" << endl;
+				cout << endl;
+			}
+			(*el)->setBoarding(false);
+			(*el)->last_floor_down = { -1, -1, -1 };
+			(*el)->last_floor_up = { -1, -1, -1 };
+		}
+	
+		
+
+		// if the elevator is moving DOWN, and a floor request matches the current floor
+		else if (((*el)->last_floor_down[DIRECTION] != -1) && (
+			
+			( ((*el)->moving_up() == NO) && ((*el)->last_floor_down[FLOOR] == (*el)->get_level()) && (*el)->ele_down.isempty() == NO )
+			
+			// OR if the elevator's UP queue is empty and it's DOWN queue is not empty, and a floor request matches the current floor
+			|| ( ( ((*el)->ele_up.isempty() == YES) && (*el)->ele_down.isempty() == NO ) && ((*el)->last_floor_up[FLOOR] == (*el)->get_level()) )
+
+			// OR if the elevator is now stationary, but it had a DOWN request, and a floor request matches the current floor
+			|| ( ((*el)->is_stationary() == YES) && ((*el)->last_floor_down[FLOOR] == (*el)->get_level()) ) ) )
+
+		{
+			if (DEBUG == YES)
+			{
+				cout << endl;
+				cout << "***Elevator Starts Boarding***" << endl;
+				cout << "Elevator: " << (*el)->get_name() << " stops at " << (*el)->get_level() << endl;
+				cout << "Request floor: " << (*el)->last_floor_down[FLOOR] << endl;
+				cout << "Request direction: " << (*el)->last_floor_down[DIRECTION] << endl;
+				cout << "Request time: " << (*el)->last_floor_down[TIME] << endl;
+				cout << "Current time: " << current_time << endl;
+				cout << "Waiting time: " << current_time - (*el)->last_floor_down[TIME] << endl;
+				cout << endl;
+			}
+		
+
+
+			vector<User*> users = generateUsers((*el)->get_level(), (*el)->last_floor_down[DIRECTION], number_of_floors, total_users);
+			printUserStatus(users);
+
+
+			for (int index = 0; index < users.size(); index++)
+			{
+				// vector for each users { FLOOR, DIRECTION, which matches the elevator's floor request direction, and the current time }
+				vector<int> eachUserRequest = { users[index]->get_userDestination(), (*el)->last_floor_down[DIRECTION], current_time };
+
+				(*el)->ele_down.push_down(eachUserRequest);
+
+				waiting_times.push_back(current_time - (*el)->last_floor_down[TIME]); // add waiting time to vector
+
+
+				if (DEBUG == YES)
+				{
+					cout << "User " << users[index]->get_UserID() << " entered Elevator " << (*el)->get_name() << endl;
+				}
+			}
+
+			// Delete users vector (dynamic memory)
+			deleteVector(users);
+
+			(*el)->ele_down.pop_front(); // removes request from the queue
+
+			(*el)->setBoarding(true);
+		}
+
+		// if the elevator is moving UP and a floor request matches the current floor
+		else if (((*el)->last_floor_up[DIRECTION] != -1) && (
+			
+			(((*el)->moving_up() == YES) && ((*el)->last_floor_up[FLOOR] == (*el)->get_level())  )
+
+			|| // OR  if the elevator's DOWN queue is empty and it's UP queue is not empty, and a floor request matches the current floor
+			((((*el)->ele_down.isempty() == YES) && (*el)->ele_up.isempty() == NO) && ((*el)->last_floor_up[FLOOR] == (*el)->get_level()) )
+
+			|| // OR  if the elevator is now stationary, but it had an UP request 
+			(((*el)->is_stationary() == YES) && ((*el)->last_floor_up[FLOOR] == (*el)->get_level())) ) )
+
+		{
+			if (DEBUG == YES)
+			{
+				cout << endl;
+				cout << "***Elevator Starts Boarding***" << endl;
+				cout << "Elevator: " << (*el)->get_name() << " stops at " << (*el)->get_level() << endl;
+				cout << "Request floor: " << (*el)->last_floor_up[FLOOR] << endl;
+				cout << "Request direction: " << (*el)->last_floor_up[DIRECTION] << endl;
+				cout << "Request time: " << (*el)->last_floor_up[TIME] << endl;
+				cout << "Current time: " << current_time << endl;
+				cout << "Waiting time: " << current_time - (*el)->last_floor_up[TIME] << endl;
+				cout << endl;
+			}
+
+
+
+
+
+
+			vector<User*> users = generateUsers((*el)->get_level(), (*el)->last_floor_up[DIRECTION], number_of_floors, total_users);
+			printUserStatus(users);
+
+
+			for (int index = 0; index < users.size(); index++)
+			{
+				// vector for each users { FLOOR, DIRECTION, which matches the elevator's floor request direction, and the current time }
+				vector<int> eachUserRequest = { users[index]->get_userDestination(), (*el)->last_floor_up[DIRECTION], current_time };
+
+				(*el)->ele_up.push_up(eachUserRequest);
+
+				waiting_times.push_back(current_time - (*el)->last_floor_up[TIME]); // add waiting time to vector
+
+				if (DEBUG == YES)
+				{
+					cout << "User " << users[index]->get_UserID() << " entered Elevator " << (*el)->get_name() << endl;
+				}
+			}
+
+			(*el)->ele_up.pop_front(); // removes request from the queue
+
+			// Delete users vector (dynamic memory)
+			deleteVector(users);
+
+			//(*el)->up_stops();
+			//cout << "\t\t Elevator " << (*el)->get_name() << "\t\t on floor " << (*el)->get_level() << endl;
+			(*el)->setBoarding(true);
+		}
+
+		
+// ELEVATOR MOVEMENT LOGIC //
+
+		else if ((*el)->ele_down.getsize() == 0 && (*el)->ele_up.getsize() == 0) {
+			cout << "initial condition" << endl;
+		}
+		else if ((*el)->ele_down.getsize() != 0 && (*el)->ele_up.getsize() == 0) {
+			(*el)->down_stops();
+			cout << "\t\t Elevator " << (*el)->get_name() << "\t\t on floor " << (*el)->get_level() << endl;
+		}
+		else if ((*el)->ele_down.getsize() == 0 && (*el)->ele_up.getsize() != 0) {
+			(*el)->up_stops();
+			cout << "\t\t Elevator " << (*el)->get_name() << "\t\t on floor " << (*el)->get_level() << endl;
+		}
+		else if ((*el)->ele_up.getsize() != 0 && (*el)->moving_up() == true) {
+			(*el)->up_stops();
+			cout << "\t\t Elevator " << (*el)->get_name() << "\t\t on floor " << (*el)->get_level() << endl;
+		}
+		else if ((*el)->ele_down.getsize() != 0 && (*el)->moving_up() == false) {
+			(*el)->down_stops();
+			cout << "\t\t Elevator " << (*el)->get_name() << "\t\t on floor " << (*el)->get_level() << endl;
+		}
+		else if ((*el)->ele_up.getsize() != 0 && (*el)->moving_up() == false) {
+			(*el)->set_direction(true);
+		}
+		else if ((*el)->ele_down.getsize() != 0 && (*el)->moving_up() == true) {
+			(*el)->set_direction(false);
+		}
+		else { cout << "Nothing happend " << endl; }
+	}
+}
+
+
+void calculateDistance(vector<Elevator*>& elevators, int task)
+{
+	// Calculates elevators' distances from requested floor
+	for (int index = 0; index < elevators.size(); index++)
+	{
+		elevators[index]->set_distanceFromRequest(task);
+
+		if (DEBUG == YES)
+		{
+			cout << "Elevator " << index << " distance from request: " <<
+				elevators[index]->get_distanceFromRequest() << endl;
+		}
+	}
+}
---
+++ FloorRequests.cpp
@@ -0,0 +1,69 @@
+#include "FloorRequests.h"
+#include "Utilities.h"
+#include <vector>
+
+/*
+floorRequest generates a random floor request and a direction 
+			 and returns a pointer to a 2-index array
+*/
+
+int* floorRequest(const int& number_of_floors)
+{	
+	int floorDirection; // 0 = DOWN, 1 = UP
+	int floorNumber;
+	int newFloorRequest[2];
+	int isRequest = rand() % 2;
+	//int isRequest = true;
+
+	// No floor request was made
+	if (isRequest == NO)
+	{
+		floorDirection = -1;
+		floorNumber = -1;
+	}
+
+	// A floor request was made
+	else if (isRequest == YES)
+	{
+		// Random floor generated
+		int currentFloor = 1 + rand() % number_of_floors;
+
+		if (currentFloor == 0) // if on ground floor, have to go up
+		{
+			floorDirection = UP;
+		}
+
+		else if (currentFloor == number_of_floors) // if on top floor, have to go down
+		{
+			floorDirection = DOWN;
+		}
+
+		else
+		{
+			// generates floor direction, down or up (0 or 1)
+			//floorDirection = rand() % 2;
+			floorDirection = randomFloor(1, 0);
+		}
+
+		if (floorDirection == DOWN) 
+		{
+			// generates floor requests from below current floor to bottom floor
+
+			floorNumber = randomFloor(currentFloor - 1, 0);
+		}
+
+
+		else if (floorDirection == UP) 
+		{
+			// generates floor requests from above current floor to top floor
+			floorNumber = randomFloor(number_of_floors , currentFloor + 1);
+		}
+	}
+
+	// Add to array
+	newFloorRequest[DIRECTION_REQUEST] = floorDirection;
+	newFloorRequest[FLOOR_REQUEST] = floorNumber;
+	//newFloorRequest[DIRECTION] = 0;
+
+	return newFloorRequest;
+}
