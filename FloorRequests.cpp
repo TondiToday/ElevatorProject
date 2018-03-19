182e5f7057b9d55ddf3a2d96f2bf4538e9e3fac6
Stirk91 <linden.the.lionheart@gmail.com>
2018-03-19 09:42:51 -0500

    Sim, Utilities fixes

--- FloorRequests.cpp
+++ FloorRequests.cpp
@@ -48,7 +48,6 @@ int* floorRequest(const int& number_of_floors)
 		if (floorDirection == DOWN) 
 		{
 			// generates floor requests from below current floor to bottom floor
-
 			floorNumber = randomFloor(currentFloor - 1, 0);
 		}
 
